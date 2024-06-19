99fe86158621e2c6d77c408c4d256fcd37b56081
vahid <vahidkosari28@gmail.com>
2024-06-19 07:47:19 +0200

    add InsertionSort

--- CLRS/Program.cs
+++ CLRS/Program.cs
@@ -1,7 +1,16 @@
-﻿public class Program
+﻿using CLRS;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        List<double> list = new() { 1, 100, 4, 10.5, 324, 10, 10, 4, 8};
+        SortAlgorithms.InsertionSort(list);
+
+        foreach (double d in list)
+        {
+            Console.WriteLine(d); 
+        }
+
     }
 }
---
+++ CLRS/SortAlgorithms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLRS
+{
+    public static class SortAlgorithms
+    {
+        public static void InsertionSort(IList<double> list)
+        {
+            for(int i = 1; i < list.Count; i++)
+            {
+                double key = list[i];
+                int j = i - 1;
+                while(j >= 0 && key < list[j])
+                {
+                    list[j+1] = list[j];
+                    j--;
+                }
+                list[j + 1] = key;
+            }
+        }
+    }
+}
